ee70d224cf483c9365038ad0585d874f7b8763f2
Reptar007 <sebe4106@gmail.com>
2023-04-11 16:19:51 -0400

    Add User login backend endpoint

---
+++ backend/routes/api/session.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const { setTokenCookie, restoreUser } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+
+const router = express.Router();
+
+router.post("/", async (req, res, next) => {
+  const { credential, password } = req.body;
+
+  const user = await User.login({ credential, password });
+
+  if (!user) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = ["The provided credentials were invalid."];
+    return next(err);
+  }
+
+  const token = await setTokenCookie(res, user);
+
+  const body = {
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    username: user.username,
+    profilePicture: user.profilePicture,
+    token,
+  };
+
+  return res.json(body);
+});
+
+module.exports = router;
