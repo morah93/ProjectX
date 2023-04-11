dbbf729759ae33beed2fd31e16b0b059835325d0
Reptar007 <sebe4106@gmail.com>
2023-04-11 16:37:43 -0400

    Add User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,28 @@
 const express = require("express");
+
+const { setTokenCookie, restoreUser } = require("../../utils/auth.js");
+const { User } = require("../../db/models");
+
 const router = express.Router();
+
+// Sign up
+router.post("/", async (req, res, next) => {
+    const { email, password, username, firstName, lastName } = req.body;
+    const user = await User.signup({ email, username, password, firstName, lastName });
+
+    const token = await setTokenCookie(res, user);
+
+    const body = {
+        id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+        profilePic: user.profilePic,
+        token
+    };
+
+    return res.json(body);
+});
 
 module.exports = router;
