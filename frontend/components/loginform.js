9ae9c90cd7214845fabd966d6a78b31b4398b8f4
Melody Oberg <melody@Melodys-iMac.local>
2023-04-15 21:20:47 -0700

    initial start of frontend

---
+++ frontend/components/loginform.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loginform.css"
+
+
+const LoginForm = () => {
+    return (
+        <div className="login">
+            <h1>Login</h1>
+            <input type="button"></input>
+        </div>
+    )
+}
+
+export default LoginForm
---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import LoginForm from "./components/loginform"
+
+
+function App() {
+    return (
+        <div className="login-nav">
+            <LoginForm />
+        </div>
+    )
+}
